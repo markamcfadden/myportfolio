bd3d3b568e70555476c85acb9b67302007b8a792
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-19 15:23:54 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ProjectsPage from "./pages/ProjectsPage";
 import ContactPage from "./pages/ContactPage";
+import AboutMePage from "./pages/AboutMePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/aboutme" element={<AboutMePage />} />
       <Route path="/projects" element={<ProjectsPage />} />
       <Route path="/contact" element={<ContactPage />} />
     </Routes>
