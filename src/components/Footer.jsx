0c818313909eedf1933fa7345a24ae85c4a0e233
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-20 14:14:19 -0300

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+function Footer() {
+  return <FooterContainer></FooterContainer>;
+}
+
+export default Footer;
+
+const FooterContainer = styled.div`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  width: 90%;
+  max-width: 1500px;
+  padding: 40px;
+  background-color: grey;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
+`;
