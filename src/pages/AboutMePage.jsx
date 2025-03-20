be326dc509dee6c99c928a7776e747afbd08349e
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-20 14:18:49 -0300

    page layout design

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import Biography from "../components/Biography";
 
 function AboutMePage() {
   return (
     <PageContainer>
       <Navbar />
-      <Biography />
+      <AboutMeContainer>
+        <Biography />
+      </AboutMeContainer>
+      <Footer />
     </PageContainer>
   );
 }
@@ -19,4 +23,15 @@ const PageContainer = styled.div`
   align-items: center;
   min-height: 100vh;
   background-color: white;
+`;
+
+const AboutMeContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 25px;
+  width: 100%;
+  max-width: 1500px;
+  border: 3px solid green;
 `;
