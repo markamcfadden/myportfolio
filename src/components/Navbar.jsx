192f5861eed18c1dba7cd6e2ff1a23c4d603a674
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-20 23:03:23 -0300

    add responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const NavbarContainer = styled.nav`
   margin-top: 20px;
   margin-bottom: 20px;
   width: 90%;
-  max-width: 1500px;
+  max-width: 1600px;
   padding: 40px;
   background-color: grey;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
