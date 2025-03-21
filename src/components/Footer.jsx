dc0b554b4208ea6637fc77c3c73bc478647f44f2
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-20 23:03:01 -0300

    add responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const FooterContainer = styled.div`
   margin-top: 20px;
   margin-bottom: 20px;
   width: 90%;
-  max-width: 1500px;
+  max-width: 1600px;
   padding: 40px;
   background-color: grey;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
