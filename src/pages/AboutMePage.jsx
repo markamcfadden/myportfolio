44e2e64e088a8534ec1438be645b294a79160f3e
markamcfadden <mcfadden.a.mark@gmail.com>
2025-04-08 16:02:17 -0300

    edit styling

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -9,15 +9,24 @@ function AboutMePage() {
   return (
     <PageContainer>
       <Navbar />
-      <TerminalIntro />
       <AboutMeContainer>
         <LeftSide>
           <ProfilePhoto />
           <DownloadLink href="cvlink" download>
             Download My CV
           </DownloadLink>
+          <SocialButton href="https://github.com/markamcfadden" target="_blank">
+            GitHub
+          </SocialButton>
+          <SocialButton
+            href="https://www.linkedin.com/in/mark-mcfadden-226a821b3/"
+            target="_blank"
+          >
+            LinkedIn
+          </SocialButton>
         </LeftSide>
         <RightSide>
+          <TerminalIntro />
           <Biography />
         </RightSide>
       </AboutMeContainer>
@@ -39,11 +48,11 @@ const PageContainer = styled.div`
 const AboutMeContainer = styled.div`
   flex-grow: 1;
   display: flex;
-  align-items: center;
+  align-items: stretch;
   justify-content: center;
   padding: 25px;
-  width: 100%;
   max-width: 1500px;
+  gap: 25px;
 `;
 
 const LeftSide = styled.div`
@@ -51,9 +60,14 @@ const LeftSide = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 20px;
 `;
 
 const RightSide = styled.div`
+  width: 850px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   flex-grow: 1;
 `;
 
@@ -61,13 +75,33 @@ const DownloadLink = styled.a`
   background-color: black;
   color: white;
   padding: 15px;
-  min-width: 80px;
+  min-width: 160px;
   border: none;
   border-radius: 8px;
   cursor: pointer;
   text-decoration: none;
   font-size: 1rem;
   transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #00ff88;
+    color: black;
+  }
+`;
+
+const SocialButton = styled.a`
+  display: flex;
+  background-color: black;
+  color: white;
+  padding: 15px;
+  min-width: 160px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1rem;
+  transition: background-color 0.3s;
+  justify-content: center;
 
   &:hover {
     background-color: #00ff88;
