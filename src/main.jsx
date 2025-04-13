12b7948ad4a71343e7530d9a231653bf319b923f
markamcfadden <mcfadden.a.mark@gmail.com>
2025-04-13 16:29:20 -0300

    fix router basename for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import GlobalStyles from "./GlobalStyles.js";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/myportfolio">
     <GlobalStyles />
     <App />
   </BrowserRouter>
